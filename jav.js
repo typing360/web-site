78262be52d3e49edfffdce06d4c853c3d91ef56a
typing360 <mwamjombam@gmail.com>
2026-09-10 15:47:58 +0300

    new coder

---
+++ jav.js
@@ -0,0 +1,6 @@
+function coder() {
+    document.getElementById('h2').style.fontFamily="arial"
+      document.getElementById('h2').style.fontSize="30px"
+        document.getElementById('h2').style.fontWeight="200px"
+    
+}
